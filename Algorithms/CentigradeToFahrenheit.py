b508db9331e04d4b2d4c48916595c63ea66af86f
Dr. Murtaza Ali Khan <drkhanmurtaza@gmail.com>
2024-10-18 22:29:35 +0400

    Add files via upload

---
+++ Algorithms/CentigradeToFahrenheit.py
@@ -0,0 +1,15 @@
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+# Example usage
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = celsius_to_fahrenheit(celsius)
+print(f"{celsius}°C is equal to {fahrenheit}°F.")
+
+## ResearchGate: http://www.researchgate.net/profile/Murtaza_Khan2/
+## LinkedIn: https://www.linkedin.com/in/dr-murtaza-ali-khan-3b368019
+## Google Scholar: https://scholar.google.com/citations?user=n0JDQ0sAAAAJ
+## Scopus: https://www.scopus.com/authid/detail.uri?authorId=7410318323
+## GitHub: https://github.com/drmurtazakhan
